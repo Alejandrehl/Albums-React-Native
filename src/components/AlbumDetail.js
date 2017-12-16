660dbe7d7e041f3d9c230311da919ec0c125e0c1
Alejandro Hernández <Alejandrehl@icloud.com>
2017-12-15 21:34:37 -0300

    Album images

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,17 +1,60 @@
 import React, { Component } from 'react';
 import {
   Text,
-  View
+  View,
+  Image
 } from 'react-native';
-import axios from 'axios';
+import Card from './Card';
+import CardSection from './CardSection';
 
 export default class AlbumDetail extends Component<{}> {
 
   render() {
     return (
-      <View>
-      </View>
+      <Card>
+      	<CardSection>
+      		<View style={styles.thumbnailContainerStyle}>
+      			<Image 
+      				source={{ uri: this.props.album.thumbnail_image }} 
+      				style={styles.thumbnailStyle}/>
+      		</View>
+      		<View style={styles.headerContentStyle}>
+      			<Text style={styles.headerTextStyle}>{this.props.album.title}</Text>
+      			<Text>{this.props.album.artist}</Text>
+      		</View>
+      	</CardSection>
+      	<CardSection>
+      		<Image 
+      			source={{ uri: this.props.album.image }}
+      			style={styles.imageStyle}/>
+      	</CardSection>
+      </Card>
     );
   }
 
-}
+};
+
+const styles = {
+	headerContentStyle: {
+		flexDirection: 'column',
+		justifyContent: 'space-around'
+	},
+	headerTextStyle: {
+		fontSize: 18
+	},
+	thumbnailStyle: {
+		height: 50,
+		width: 50
+	},
+	thumbnailContainerStyle: {
+		justifyContent: 'center',
+		alignItems: 'center',
+		marginLeft: 10,
+		marginRight: 10
+	},
+	imageStyle: {
+		height: 300,
+		flex: 1,
+		width: null
+	}
+} 
--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -4,7 +4,7 @@ import {
   View
 } from 'react-native';
 import axios from 'axios';
-import AlbumDetail from 'AlbumDetail';
+import AlbumDetail from './AlbumDetail';
 
 export default class AlbumList extends Component<{}> {
   state = { albums: [] };
@@ -17,7 +17,7 @@ export default class AlbumList extends Component<{}> {
 
   renderAlbums(){
     return this.state.albums.map(album => 
-      <Text key={album.title}>{album.title}</Text>
+      <AlbumDetail key={album.title} album={album}/>
       );
   }
 
